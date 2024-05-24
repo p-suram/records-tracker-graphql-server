a4af31163f21ac48f12884ddf016cd1ed9c7b901
p-suram <poojasuram07@gmail.com>
2024-05-24 12:19:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,120 @@
+const { ApolloServer } = require("@apollo/server");
+const { createServer } = require("http");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const { PubSub } = require("graphql-subscriptions");
+
+const port = 4000;
+
+const records_in_radius = "RECORDS_IN_RADIUS";
+
+const pubSub = new PubSub();
+
+
+const typeDefs = `
+    type Address {
+        location: String!
+        latitude: Float!
+        longitude: Float!
+    }
+    type CaseDetail {
+        type: String!
+        date: String!
+    }
+    type Record {
+        firstName: String!
+        lastName: String!
+        middleName: String
+        age: Int!
+        dob: String!
+        address: Address!
+        caseDetail: CaseDetail!
+    }
+    type Query {
+        getRecordsInRadius(latitude: Float!, longitude: Float!, radiusInMiles: Float!): [Record]
+    }
+
+    type Subscription {
+        recordInRadius: [Record]!
+    }
+`;
+
+const getAllRecordsInRadiusAndPublish = async (latitude, longitude, radiusInMiles) => {
+    try {
+        console.log("making api to homicide");
+        const response = await fetch(`http://localhost:8080/homicide-records/records-in-radius?latitude=${latitude}&longitude=${longitude}&radiusInMiles=${radiusInMiles}`);
+        const data = await response.json();
+        console.log("response from api", data);
+        pubSub.publish(records_in_radius, {
+            recordInRadius: data});
+    } catch (error) {
+        console.error("Error fetching greeting:", error);
+    }
+}
+
+const resolvers = {
+  Query: {
+    getRecordsInRadius (parent, args, context, info) {
+        const { latitude, longitude, radiusInMiles } = args;
+        getAllRecordsInRadiusAndPublish(latitude, longitude, radiusInMiles);
+        return [];
+    }
+  },
+  Subscription: {
+    recordInRadius: {
+        subscribe: () => pubSub.asyncIterator([records_in_radius]),
+    },
+  },
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const app = express();
+
+app.use(cors());
+
+const httpServer = createServer(app);
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/graphql",
+});
+
+const wsServerCleanup = useServer({ schema }, wsServer);
+
+const apolloServer = new ApolloServer({
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await wsServerCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
+
+(async function () {
+  await apolloServer.start();
+  app.use("/graphql", bodyParser.json(), expressMiddleware(apolloServer));
+})();
+
+httpServer.listen(port, () => {
+  console.log(`🚀 Query endpoint ready at http://localhost:${port}/graphql`);
+  console.log(
+    `🚀 Subscription endpoint ready at ws://localhost:${port}/graphql`
+  );
+});
